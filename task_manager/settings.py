085a2d393f97434d775a578adf29209f583cff7d
Oleksandr Leha <legus96@gmail.com>
2023-04-02 17:11:31 +0300

    Updated settings and requirements

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b(*k=ppcyz)-umf@y72%w-zk2j9nosh_@8i+@ve4sm-ovegz=d'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-b(*k=ppcyz)-umf@y72%w-zk2j9nosh_@8i+@ve4sm-ovegz=d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
